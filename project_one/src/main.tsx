bfdc90d9fae0b99591fffd1483e2304ff1d4abf3
PaddyHi <patrickhinlopen@gmail.com>
2025-05-24 21:18:33 +0200

    updateP1

--- project_one/src/main.tsx
+++ project_one/src/main.tsx
@@ -5,9 +5,8 @@ import './index.css';
 import App from './App.tsx';
 import { Auth0Provider } from '@auth0/auth0-react';
 
-const domain = {{Project1.VITE_AUTH0_DOMAIN}};
-
-const clientId = {{Project1.VITE_AUTH0_CLIENT_ID}};
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
 
 // Debug logging
 console.log('Auth0 Config:', { domain, clientId });
